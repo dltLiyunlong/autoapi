c0cb14c1d77c09d5f615f08cf0991c4807b8f636
dltLiyunlong <316396152@qq.com>
2021-01-25 17:33:45 +0800

    测试提交

--- testcase/zz.py
+++ testcase/zz.py
@@ -5,7 +5,8 @@ request = RunMethod.run_main
 data = ReadData().get_excel('/testcase.xls', 'test')
 yaml_data = ReadData().get_yaml()
 
-req = request(str(data[0][2]),url=yaml_data['dr_jg_host'] + data[0][4], data=data[0][6],
-              headers=data[0][5])
 
-print(req)
+
+print(data)
+
+
