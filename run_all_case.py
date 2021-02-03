8ef3d53768755106c8ae4ce117d38be3ace69aff
dltLiyunlong <316396152@qq.com>
2021-02-03 21:06:16 +0800

    修改执行方式

--- run_all_case.py
+++ run_all_case.py
@@ -3,9 +3,6 @@ import pytest
 import allure
 
 if __name__ == '__main__':
-    pytest.main(['-s', ''])
-    pytest.main(['-s', '-q', '--alluredir', 'F:/autop/reports/result', ])
-    split = 'allure ' + 'generate ' + 'F:/autop/reports/result ' + '-o ' + 'F:/autop/reports/html ' + '--clean'
-    os.system('cd F:/autop/reports')
-    os.system(split)
-    print(split)
+    pytest.main(["-sq",
+                 "--alluredir", "./allure-results"])
+    os.system(r"allure generate --clean allure-results -o allure-report")
